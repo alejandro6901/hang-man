e02c795240d940f710547c1fbb14dfcf2f1b3218
Alejandro Lopez <alejandro_9601@hotmail.com>
2016-07-26 19:30:38 -0600

    split

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
-var hang-man = {
- word:'Prueba',
- newword: function functionName(word) {
+// var hang-man = {
+//  word:'Prueba',
+//  newword: function functionName(word) {
 
-  }
+//   }
+// }
+  var word= 'Prueba';
+function functionName(word){
+	for (var i = 0; i < word.length; i++) {
+		
+    console.log(word.charAt(i));
+
+}
+console.log(word[3]); 
 }
 
-
-function functionName() {
-  hang-man.newword();
-}
+// function functionName() {
+//   hang-man.newword();
+// }
